6500273d15c8949dc1a781d447df61bfafefce47
ZhangYao <819647113@qq.com>
2021-09-23 09:56:18 +0800

    [ADD]刷题+1

--- src/main/java/com/aiz/leetcode/practice/Solution13.java
+++ src/main/java/com/aiz/leetcode/practice/Solution13.java
@@ -1,12 +1,22 @@
 package com.aiz.leetcode.practice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @ClassName Solution13
- * @Description 罗马数字转整数
+ * @Description 罗马数字转整数 https://leetcode-cn.com/problems/roman-to-integer/
  * @Author Yao
  * @Date Create in 11:03 下午 2021/9/21
  * @Version 1.0
  */
 public class Solution13 {
 
+    public int romanToInt(String s) {
+        Map map = new HashMap<String, String>(1);
+        map.put("","");
+        int result = 0;
+
+        return result;
+    }
 }
