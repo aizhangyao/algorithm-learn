0183c32929beaa5fd58e8c21157018593ce3cf21
zhangyao <zhangyao_xz@qq.com>
2023-04-05 22:11:21 +0800

    add ⚔ Offer 06. 从尾到头打印链表

---
+++ src/main/java/com/aiz/lc/offer/day02/SolutionOffer06.java
@@ -0,0 +1,40 @@
+package com.aiz.lc.offer.day02;
+
+import java.util.Stack;
+
+/**
+ * @author ZhangYao
+ * @version 1.0
+ * @className SolutionOffer06
+ * @description 剑指 Offer 06. 从尾到头打印链表
+ * https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+ * @date Create in 19:08 2023/4/4
+ */
+public class SolutionOffer06 {
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] result = new int[stack.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+
+
+}
