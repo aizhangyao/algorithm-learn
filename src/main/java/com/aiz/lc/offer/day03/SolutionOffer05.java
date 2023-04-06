58ef7abf7153fbce5ea8cf3c2e3a6c88885133b8
zhangyao <zhangyao_xz@qq.com>
2023-04-06 10:00:58 +0800

    add ⚔ Offer 05. 替换空格

---
+++ src/main/java/com/aiz/lc/offer/day03/SolutionOffer05.java
@@ -0,0 +1,15 @@
+package com.aiz.lc.offer.day03;
+
+/**
+ * @author ZhangYao
+ * @version 1.0
+ * @className SolutionOffer05
+ * @description 剑指 Offer 05. 替换空格
+ * https://leetcode.cn/problems/ti-huan-kong-ge-lcof/
+ * @date Create in 09:43 2023/4/6
+ */
+public class SolutionOffer05 {
+    public String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+}
