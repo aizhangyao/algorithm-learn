8c7d0eecf5930544c84b2bc2e2a0e6916276313b
zhangyao <zhangyao_xz@qq.com>
2025-02-08 00:26:29 +0800

    add 买卖股票的最佳时机 II

--- src/main/java/com/aiz/leetcode/practice/买卖股票的最佳时机II.java
+++ src/main/java/com/aiz/leetcode/practice/买卖股票的最佳时机II.java
@@ -1,14 +1,14 @@
 package com.aiz.leetcode.practice;
 
+import org.junit.jupiter.api.Test;
+
 /**
- * @ClassName Solution122
  * @Description 买卖股票的最佳时机 II
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
- * @Author Yao
  * @Date Create in 5:31 下午 2021/10/1
  * @Version 1.0
  */
-public class Solution122 {
+public class 买卖股票的最佳时机II {
     public static int maxProfit(int[] prices) {
         int profit = 0;
         for (int i = 1; i < prices.length; i++) {
@@ -20,18 +20,15 @@ public class Solution122 {
         return profit;
     }
 
-    public static void test1() {
+    @Test
+    public void test1() {
         int[] prices = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit(prices));
     }
 
-    public static void test2() {
+    @Test
+    public void test2() {
         int[] prices = {1, 2, 3, 4, 5};
         System.out.println(maxProfit(prices));
-    }
-
-    public static void main(String[] args) {
-        test1();
-        test2();
     }
 }
